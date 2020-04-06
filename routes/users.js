3fe49ec0a068fbfff7197fdf199fdf5cb92b6c5c
anaisperson <person.anais@gmail.com>
2020-04-06 23:45:57 +0200

    clean all code

--- routes/users.js
+++ routes/users.js
@@ -3,22 +3,38 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require("../models/User");
 
+router.get("/signup", function(req, res, next) {
+    res.render('signup', { error: req.query.error });
+});
+
 router.post("/signup", function(req, res, next) {
     req.body.password = bcrypt.hashSync(req.body.password, 10);
+
+    const verifMail = User.findOne({ email: req.body.email })
+        .then((user) => {
+            if (user) {
+                const error = encodeURIComponent('This email is already used !');
+                res.redirect(`/users/signup?error=${error}`);
+            }
+        });
 
     const newUser = User(req.body);
     newUser.save()
         .then((user) => {
             if (user !== newUser) {
-                res.status(500).send({ message: 'Error on user signup !' });
-                return;
+                const error = encodeURIComponent('Error on user signup !');
+                res.redirect(`/users/signup?error=${error}`);
             }
-            res.status(200).send(user);
+            res.redirect('/users/login');
         })
         .catch((err) => {
             console.error(err);
             res.status(500).send({ message: 'System error on signup !' });
         });
+});
+
+router.get("/login", function(req, res, next) {
+    res.render('login');
 });
 
 router.post("/login", function(req, res, next) {
@@ -32,7 +48,7 @@ router.post("/login", function(req, res, next) {
             }
 
             req.session.user = user;
-            res.send({ message: 'login !' });
+            res.redirect('/users/profile');
         })
         .catch((err) => {
             console.error(err);
@@ -42,15 +58,15 @@ router.post("/login", function(req, res, next) {
 
 router.get("/logout", function(req, res, next) {
     req.session.destroy();
-    res.send({ message: 'logout' })
+    res.redirect('/');
 });
 
 router.get("/profile", function(req, res, next) {
     if (!req.session.user) {
-        return res.status(401).send({ message: 'Please login before !' });
+        res.redirect('/users/login');
     }
 
-    res.status(200).send(req.session.user);
+    res.render('dashboard', { user: req.session.user });
 });
 
 module.exports = router;
