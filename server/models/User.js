1916ea84737dd5e4adbbe1f6e307072fc3f67c1d
anaisperson <person.anais@gmail.com>
2020-04-06 17:10:19 +0200

    clean code and signup/login route

--- server/models/User.js
+++ server/models/User.js
@@ -1,20 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-  {
+const userSchema = new Schema({
     email: { type: String, required: true, unique: true },
-    firstname: { type: String },
-    lastname: { type: String },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
     password: { type: String, required: true }
-  },
-  {
+}, {
     timestamps: {
-      createdAt: "created_at",
-      updatedAt: "updated_at"
+        createdAt: "created_at",
+        updatedAt: "updated_at"
     }
-  }
-);
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,89 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const User = require("../models/User");
 
 router.post("/signup", function(req, res, next) {
-  const { firstname, lastname, email, password } = req.body;
-  console.log(req.body);
-  console.log(firstname, " --- ", lastname, " --- ", email, " --- ", password);
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
 
-  res.status(200).send("ok");
+    const newUser = User(req.body);
+    newUser.save()
+        .then((user) => {
+            if (user !== newUser) {
+                res.status(500).send({ message: 'Error on user signup !' });
+                return;
+            }
+            res.status(200).send(user);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send({ message: 'System error on signup !' });
+        });
 });
 
-router.post("/login", function(req, res, next) {});
+router.post("/login", function(req, res, next) {
+
+    User.findOne({ email: req.body.email })
+        .then((user) => {
+            if (!user) {
+                return res.status(400).send({ message: "This email does not exist" });
+            }
+            if (!bcrypt.compareSync(req.body.password, user.password)) {
+                return res.status(400).send({ message: "The password is invalid" });
+            }
+
+            res.send({ message: "The username and password combination is correct!" });
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send({ message: 'System error on login !' });
+        });
+});
 
 router.get("/logout", function(req, res, next) {});
 
 router.get("/profile", function(req, res, next) {});
-
-/* /users/ */
-router.get("/", function(req, res, next) {
-  console.log(req.session);
-  if (req.session.currentUser) {
-    res.redirect("/users/dashboard");
-  } else {
-    res.redirect("/users/login");
-  }
-
-  res.status(200).render("login", { style: login_style });
-
-  // UserModel.find(function(err, usersList) {
-  //   res.render('index', {title: usersList}, function (err, html) {
-  //     res.send(html)
-  //   })
-  // });
-});
-
-// SIGNUP
-router.get("/new", function(req, res, next) {
-  res.render("signup");
-});
-
-// router.post("/new") à créer pour ajouter de nouveaux users
-
-// LOGIN
-router.get("/login", function(req, res, next) {
-  // if (req.session.currentUser) {
-  //   res.status(200).render('dashboard', {style: dashboard_style});
-  // }
-  res.render("login");
-});
-
-router.post("/login", function(req, res, next) {
-  const theUsername = req.body.username;
-  const thePassword = req.body.password;
-
-  if (theUsername === "" || thePassword === "") {
-    res.redirect("/users/login");
-    return;
-  }
-
-  res.redirect("/users/dashboard");
-
-  User.findOne({ username: theUsername })
-    .then(user => {
-      if (!user) {
-        res.render("auth/login", {
-          errorMessage: "The username doesn't exist."
-        });
-        return;
-      }
-      if (bcrypt.compareSync(thePassword, user.password)) {
-        // Save the login in the session!
-        req.session.currentUser = user;
-        res.redirect("/");
-      } else {
-        res.render("login");
-      }
-    })
-    .catch(error => {
-      next(error);
-    });
-});
-
-// DASHBOARD
-router.get("/dashboard", function(req, res, next) {
-  res.status(200).render("dashboard", { style: dashboard_style });
-});
 
 module.exports = router;
