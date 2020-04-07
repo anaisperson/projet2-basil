642a75e144fe895d4a930315a4c90943b244b921
anaisperson <person.anais@gmail.com>
2020-04-07 18:47:01 +0200

    second table starting

--- models/User.js
+++ models/User.js
@@ -1,17 +1,32 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     email: { type: String, required: true, unique: true },
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
-    password: { type: String, required: true }
-}, {
+    password: { type: String, required: true },
+  },
+  {
     timestamps: {
-        createdAt: "created_at",
-        updatedAt: "updated_at"
-    }
-});
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+const rateSchema = new Schema(
+  {
+    title: { type: Number, required: true },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
