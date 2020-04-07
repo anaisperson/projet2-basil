c79cb297417f396ab845c86d2211e55a45a0c6dc
anaisperson <person.anais@gmail.com>
2020-04-07 15:28:08 +0200

    signup firstname+lastname

--- app.js
+++ app.js
@@ -6,23 +6,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
-const session = require('express-session');
+const session = require("express-session");
 
 mongoose
-    .connect("mongodb://localhost/basil-v2", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(x => {
-        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
-    })
-    .catch(err => {
-        console.error("Error connecting to mongo", err);
-    });
+  .connect("mongodb://localhost/basil-v2", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
+  });
 
 const app_name = require("./package.json").name;
 const debug = require("debug")(
-    `${app_name}:${path.basename(__filename).split(".")[0]}`
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
 );
 
 const app = express();
@@ -33,11 +35,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.raw());
 app.use(cookieParser());
-app.use(session({
-    'secret': 's3cr3t',
+app.use(
+  session({
+    secret: "s3cr3t",
     resave: false,
     saveUninitialized: false,
-}));
+  })
+);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
@@ -49,5 +53,6 @@ app.locals.title = "Basil";
 
 app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
+// app.use("/users/profile", require("./routes/users/profile"));
 
 module.exports = app;
